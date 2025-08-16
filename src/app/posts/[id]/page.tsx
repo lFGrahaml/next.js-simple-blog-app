2dc2a79bfc630564cce1d6296d8b331306120b38
lFGrahaml <Fgraham1@asu.edu>
2025-08-15 19:42:21 -0700

    Next.js refresher covering SPA, dynamic and static routes, next Links and useRouter hook navigation, and using search parameters via params and searchParams props and useParams and useSearchParams hooks

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from "next/navigation";
+import { posts } from "@/data/posts";
+
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = Number((await params).id);
+  if (!Number.isInteger(id)) {
+    notFound();
+  }
+
+  const post = posts.find(
+    (post) => post.id === Number(id),
+  );
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <h2>{post.title}</h2>
+      <p>{post.description}</p>
+    </main>
+  );
+}
